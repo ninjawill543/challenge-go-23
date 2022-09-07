80795b1398f5351ad740ef100d5ca1ee6ca36bf8
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-07 17:15:55 +0200

    fixed comb2

--- printcomb2.go
+++ printcomb2.go
@@ -7,10 +7,10 @@ func PrintComb2() {
 	b := '0'
 	c := '0'
 	d := '0'
-	for i := 0; i <= 89; i++ {
-		for j := 0; j <= 89; j++ {
+	for i := 0; i <= 99; i++ {
+		for j := 0; j <= 99; j++ {
 			if d == '9' {
-				if d > b && c > a {
+				if d > b && c >= a {
 					z01.PrintRune(a)
 					z01.PrintRune(b)
 					z01.PrintRune(' ')
@@ -23,7 +23,7 @@ func PrintComb2() {
 				c++
 			}
 			d++
-			if d > b && c > a {
+			if d > b && c >= a {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
 				z01.PrintRune(' ')
