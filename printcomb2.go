5cbf1494bc2a377b0b06d712d28c2c2385fb97b2
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-07 16:45:17 +0200

    added comb2

---
+++ printcomb2.go
@@ -0,0 +1,46 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	a := '0'
+	b := '0'
+	c := '0'
+	d := '0'
+	for i := 0; i <= 89; i++ {
+		for j := 0; j <= 89; j++ {
+			if d == '9' {
+				if d > b && c > a {
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(' ')
+					z01.PrintRune(c)
+					z01.PrintRune(d)
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+				d = '0'
+				c++
+			}
+			d++
+			if d > b && c > a {
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(' ')
+				z01.PrintRune(c)
+				z01.PrintRune(d)
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+			if d == '9' && c == '9' {
+				b++
+				d = '0'
+				c = '0'
+			}
+			if b == '9' {
+				b = '0'
+				a++
+			}
+		}
+	}
+}
