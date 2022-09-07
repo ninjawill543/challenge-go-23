2901e58c1b76ae29061ad035811355d2d68778c6
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-07 14:53:33 +0200

    added printcomb

---
+++ printcomb.go
@@ -0,0 +1,39 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	a := '0'
+	b := '0'
+	c := '0'
+	for c <= '9' {
+		c++
+		if c == '9' && b != '9' {
+			if c > b && b > a {
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(c)
+				z01.PrintRune(44)
+				z01.PrintRune(32)
+			}
+			b++
+			c = 0
+		}
+		if b == '9' {
+			a++
+			b = 0
+		}
+		if b == '9' && c == '9' {
+			b = 0
+			c = 0
+			a++
+		}
+		if c > b && b > a {
+			z01.PrintRune(a)
+			z01.PrintRune(b)
+			z01.PrintRune(c)
+			z01.PrintRune(44)
+			z01.PrintRune(32)
+		}
+	}
+}
