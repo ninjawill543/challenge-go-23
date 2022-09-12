7f7e8956a9e4951ed75b7b429459de41de380e26
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-12 16:58:41 +0200

    changed isprime

--- isprime.go
+++ isprime.go
@@ -4,9 +4,7 @@ func IsPrime(nb int) bool {
 	for i := 2; i < 1000000; i++ {
 		if nb%i == 0 && nb > 0 {
 			return false
-		} else if nb%i != 0 && nb > 0 {
-			return true
 		}
 	}
-	return false
+	return true
 }
