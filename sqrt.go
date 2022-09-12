25f6e2d58c997d0e3958dce469f2b87037a98b0f
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-12 16:15:56 +0200

    changed sqrt

--- sqrt.go
+++ sqrt.go
@@ -2,9 +2,11 @@ package piscine
 
 func Sqrt(nb int) int {
 	resultat := 0
-	for i := 0; i < 1000; i++ {
+	for i := 1; i < 1000; i++ {
 		if nb == i*i {
 			resultat = i
+		} else if nb == 1 {
+			return 1
 		} else {
 			return 0
 		}
