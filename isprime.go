41dae30858059310791de0907873d8a8fbf6df70
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-12 16:56:55 +0200

    changed isprime

--- isprime.go
+++ isprime.go
@@ -2,12 +2,10 @@ package piscine
 
 func IsPrime(nb int) bool {
 	for i := 2; i < 1000000; i++ {
-		if nb > 0 {
-			if nb%i == 0 {
-				return false
-			} else if nb%i != 0 {
-				return true
-			}
+		if nb%i == 0 && nb > 0 {
+			return false
+		} else if nb%i != 0 && nb > 0 {
+			return true
 		}
 	}
 	return false
