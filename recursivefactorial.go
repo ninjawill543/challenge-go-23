0ff61d3cb8dea2b5f783754430ef52ab9520f38a
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-12 11:53:02 +0200

    changed recursivefactorial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -6,9 +6,7 @@ func RecursiveFactorial(nb int) int {
 	} else if nb > 1 && nb < 100 {
 		return nb * RecursiveFactorial(nb-1)
 	} else if nb < 0 {
-		return 1
-	} else if nb == 0 {
-		return 1
+		return 0
 	}
 	return 0
 }
