a613bb86284136324dc73bba129ff129894cf025
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-12 12:00:30 +0200

    added iterativepower

--- iterativepower.go
+++ iterativepower.go
@@ -2,8 +2,18 @@ package piscine
 
 func IterativePower(nb int, power int) int {
 	result := 1
-	for i := 0; i < power; i++ {
-		result = result * nb
+	if nb > 0 && nb < 100 {
+		for i := 0; i < power; i++ {
+			result = result * nb
+		}
+		goto Exit
+	} else if nb == 0 {
+		result = 1
+		goto Exit
+	} else {
+		result = 0
+		goto Exit
 	}
+Exit:
 	return result
 }
