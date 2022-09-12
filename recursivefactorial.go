37fceb755b07f757a06602f058d2e05547cbb415
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-12 11:44:20 +0200

    changed recursivefactorial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,7 +3,8 @@ package piscine
 func RecursiveFactorial(nb int) int {
 	if nb == 1 {
 		return 1
-	} else if nb > 1 {
+	}
+	if nb > 1 && nb != 0 {
 		return nb * RecursiveFactorial(nb-1)
 	}
 	return 0
