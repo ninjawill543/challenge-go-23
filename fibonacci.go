e71fb15989aeb2d7685f6f751bc83382039cd137
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-12 15:23:26 +0200

    addded fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Fibonacci(index int) int {
+	if index > 2 {
+		return Fibonacci(index-1) + Fibonacci(index-2)
+	} else if index == 0 {
+		return 0
+	} else if index == 1 || index == 2 {
+		return 1
+	}
+	return 0
+}
