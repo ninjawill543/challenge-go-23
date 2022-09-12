b43e0f2717ebff225d52a489c37e3f459ae4b779
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-12 10:40:44 +0200

    updated iterativefact

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,13 +3,11 @@ package piscine
 func IterativeFactorial(nb int) int {
 	result := 1
 	for i := 1; i <= nb; i++ {
-		if nb < 0 || nb > 10 {
-			result = 0
-			goto Exit
-		} else {
+		if nb > 0 && nb < 10 {
 			result = result * i
+		} else {
+			result = 0
 		}
 	}
-Exit:
 	return result
 }
