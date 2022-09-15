982de0bd3979c0a6cbe86325657852d6130559ed
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-15 10:12:59 +0200

    added alphacount

---
+++ alphacount.go
@@ -0,0 +1,12 @@
+package piscine
+
+func AlphaCount(s string) int {
+	var count []int
+	for _, c := range s {
+		if (c >= 65 && c <= 90) || (c >= 97 && c <= 122) {
+			count = append(count, int(c))
+		}
+	}
+	return (len(count))
+
+}
