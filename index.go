7332efc1603197f2296fbe5c0abb9f06265db94f
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-15 14:05:44 +0200

    updated index

--- index.go
+++ index.go
@@ -1,29 +1,20 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	rep := -1
-	if len(s) < len(toFind) {
-		return -1
-	} else {
-		for i := 0; i < len(s); i++ {
-			for j := 0; j < len(toFind); j++ {
-				if s[i] == toFind[j] {
-					if len(toFind) == 1 {
-						return i
-					} else {
-						var mot string
-						for k := 0; k < len(toFind); k++ {
-							if k+i < len(s) {
-								mot += string(s[i+k])
-							}
-						}
-						if mot == toFind {
-							return i
-						}
-					}
-				}
-			}
-		}
+	counta := []rune(s)
+	countb := []rune(toFind)
+	counter := 0
+	ca := 0
+	cb := 0
+	if counta[ca] == countb[cb] {
+		ca++
+		cb++
+		counter++
+	} else if counta[ca] != countb[cb] {
+		ca++
+		counter++
 	}
-	return rep
+	//exit:
+	return counter
+
 }
