47159924c9ca0d41f14ca768ac4352d922274b9b
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-15 09:59:38 +0200

    changed nrune

--- nrune.go
+++ nrune.go
@@ -5,7 +5,7 @@ func NRune(s string, n int) rune {
 	for _, c := range s {
 		count = append(count, int(c))
 	}
-	if n < 1 {
+	if n < 1 || n > (len(count)) {
 		return 0
 	} else {
 		return rune(count[(n - 1)])
