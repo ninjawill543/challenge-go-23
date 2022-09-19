635d2d9f7f868d2e1d784da3a5fbb0801703401b
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-19 15:13:55 +0200

    changed makerange

--- makerange.go
+++ makerange.go
@@ -1,8 +1,9 @@
 package piscine
 
 func MakeRange(min, max int) []int {
-	list := make([]int, (max - min))
+	list := []int(nil)
 	if min < max {
+		list := make([]int, (max - min))
 		for i := min; i < max; i++ {
 			list[i-min] = i + 1
 		}
