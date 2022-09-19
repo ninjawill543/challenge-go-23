9608a501f35f1438f7af5e9f9136bd37961ed34b
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-19 16:02:01 +0200

    added splitwhitespaces

---
+++ splitwhitespaces.go
@@ -0,0 +1,15 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	list := []string{}
+	holder := ""
+	for _, i := range s {
+		if i != 9 && i != 32 && i != 10 {
+			holder = holder + string(i)
+		} else {
+			list = append(list, holder)
+			holder = ""
+		}
+	}
+	return list
+}
