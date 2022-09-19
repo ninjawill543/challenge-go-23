f25a49ba00c5c8f1f5ce70b7e59f82f1b4e24e04
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-19 15:09:26 +0200

    changed makerange

--- makerange.go
+++ makerange.go
@@ -4,7 +4,7 @@ func MakeRange(min, max int) []int {
 	list := make([]int, (max - min))
 	if min < max {
 		for i := min; i < max; i++ {
-			list[i] = i + i
+			list[i] = i + 1
 		}
 	} else {
 		list = nil
