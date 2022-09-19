0bd896f1f62e41550fc33ffb1d921fd295015f6f
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-19 14:00:54 +0200

    changed revparams

--- revparams/main.go
+++ revparams/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	arguments := os.Args
-	for i := len(arguments); i > 1; i-- {
+	for i := len(arguments); i >= 1; i-- {
 		for _, j := range os.Args[i] {
 			z01.PrintRune(j)
 		}
