a4e4ca0c4191dd38e39f238a4ff6b9a641105a59
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-26 14:28:46 +0200

    fixed maybe

--- displayfile/main.go
+++ displayfile/main.go
@@ -7,13 +7,16 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 {
+	if len(os.Args) == 2 {
 		content, err := ioutil.ReadFile(os.Args[1])
 		if err != nil {
 			fmt.Printf("File name missing")
 			fmt.Printf("\n")
 		}
 		fmt.Printf(string(content))
+	} else if len(os.Args) > 2 {
+		fmt.Printf("Too many arguments")
+		fmt.Printf("\n")
 	} else {
 		fmt.Printf("File name missing")
 		fmt.Printf("\n")
