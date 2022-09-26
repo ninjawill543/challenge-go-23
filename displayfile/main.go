a2da0dbb8fe9551c8068608e0018d5bc163792a7
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-26 14:25:50 +0200

    fixed maybe

--- displayfile/main.go
+++ displayfile/main.go
@@ -10,10 +10,10 @@ func main() {
 	if len(os.Args) > 1 {
 		content, err := ioutil.ReadFile(os.Args[1])
 		if err != nil {
-			fmt.Print("File name missing")
+			fmt.Printf("File name missing")
 		}
 		fmt.Println(string(content))
 	} else {
-		fmt.Print("File name missing")
+		fmt.Printf("File name missing")
 	}
 }
