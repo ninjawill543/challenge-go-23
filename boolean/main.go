3828aded3f11ecce389c160f7fb7c9faa59fbeb3
N1njaWill <hansonlucas@hotmail.co.uk>
2022-09-26 13:34:42 +0200

    stupid

--- boolean/main.go
+++ boolean/main.go
@@ -32,7 +32,6 @@ func main() {
 	lengthOfArg := len(os.Args[1:])
 	EvenMsg := "I have an even number of arguments"
 	OddMsg := "I have an odd number of arguments"
-
 	if isEven(lengthOfArg) == 1 {
 		printStr(EvenMsg)
 	} else {
